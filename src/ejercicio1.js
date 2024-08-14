ffe2c39e605816f871f4f574b76186d625bd7f2c
fwdadri <areyes@fwdcostarica.com>
2024-08-14 09:33:42 -0600

    cambios

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,7 +1,11 @@
+
+
 let contador = {
   valor: 0,
+  
   siguiente: function () {
-    // Inserte el código aquí
+    this.valor++
+    return this.valor
   },
 };
 
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,25 @@
-class Contador {
-  // Inserte el código aquí
+class Contador {//las clases siempre van con mayucula, es como el molde
+//recordar ejemplo de tazas
+  constructor(valor){//a veces no es necesario 
+    this.valor = valor;
+   // this.valor=0;    
+  }
+
+sumar(){
+  this.valor++
+  return this.valor
+}
 }
 
-// Inserte el código aquí
+let instancia= new Contador(9);
+let x = new Contador(8);
 
-export { instancia }
+console.log(instancia.sumar())
+
+console.log(x.sumar())
+
+console.log(x.sumar() + (instancia.sumar()))//
+
+
+
+//export { instancia }
