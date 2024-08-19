8a3eb069d201b93c45aa126c2ed88a4e77865c57
fwdadri <areyes@fwdcostarica.com>
2024-08-19 15:14:10 -0600

    Practia 5

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 // Inserte el código aquí
 
+class Calculadora {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    sumar() {
+        return this.x + this.y;
+    }
+
+    restar() {
+        return this.x - this.y;
+    }
+
+    multiplicar() {
+        return this.x * this.y;
+    }
+
+    dividir() {
+        if (this.y === 0) {
+            throw new Error("es imposible mamawebo.");
+        }
+        return this.x / this.y;
+    }
+}
+
+export {Calculadora};
---
+++ src/scond.js
@@ -0,0 +1,8 @@
+import {Calculadora} from "./index.js";
+
+let prueba  = new Calculadora(73487,343435);
+
+console.log(prueba.multiplicar());
+console.log(prueba.sumar());
+console.log(prueba.restar());
+console.log(prueba.dividir());
